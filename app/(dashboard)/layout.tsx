4225fc927704018a594e8bf9f64945f47e510ef1
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-19 12:53:03 +0200

    style: dashboard layout initial styles

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import { PropsWithChildren } from 'react';
 
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+const layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className="grid lg:grid-cols-5">
+      <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
+        <Sidebar />
+      </div>
+      <div className="lg:col-span-4">
+        <Navbar />
+        <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
+      </div>
+    </main>
+  );
 };
 
 export default layout;
