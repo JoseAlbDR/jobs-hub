d03758b1cc5a1b7a8b42ab429d980d8e718ba822
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-19 21:08:59 +0200

    feat: types util with zod validation for createAndEditJobSchema

--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 
-export type JobType = {
+export type JobData = {
   id: string;
   createdAt: Date;
   updatedAt: Date;
@@ -25,8 +25,26 @@ export enum JobMode {
   Internship = 'practicas',
 }
 
-export enum JobLocation {
+export enum JobType {
   Presential = 'presencial',
   Remote = 'remoto',
   Hybrid = 'hibrido',
 }
+
+export const createdAndEditJobSchema = z.object({
+  position: z
+    .string()
+    .min(2, { message: 'el puesto debe de tener al menos 2 caracteres' }),
+  company: z.string().min(2, {
+    message: 'el nombre de la empresa debe de tener al menos 2 caracteres',
+  }),
+  location: z.string().min(2, {
+    message: 'la ubicación debe de tener al menos 2 caracteres',
+  }),
+  link: z.string().url('la url no es válida'),
+  status: z.nativeEnum(JobStatus),
+  mode: z.nativeEnum(JobMode),
+  type: z.nativeEnum(JobType),
+});
+
+export type CreateAndEditJobType = z.infer<typeof createdAndEditJobSchema>;
