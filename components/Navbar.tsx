03b52a8625f2c83c14985cf6e95eca60d71def14
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-19 17:10:49 +0200

    feat: add clerk locale to esES

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import LinksDropdown from './LinksDropdown';
+import { UserButton } from '@clerk/nextjs';
+import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
-  return <h2 className="text-4xl">Navbar</h2>;
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex item-center justify-between">
+      <LinksDropdown />
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
