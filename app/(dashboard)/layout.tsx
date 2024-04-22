d8df4feb2c6b75fc818a63a78503afb0a02c6d40
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-22 22:08:54 +0200

    feat: adjust styles

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,13 +4,13 @@ import { PropsWithChildren } from 'react';
 
 const layout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="grid lg:grid-cols-[250px,repeat(4,1fr)]">
+    <main className="grid lg:grid-cols-[250px,repeat(4,1fr)] ">
       <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
         <Sidebar />
       </div>
       <div className="lg:col-span-4">
         <Navbar />
-        <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
+        <div className="py-0 px-0 sm:px-8 lg:px-5 ">{children}</div>
       </div>
     </main>
   );
