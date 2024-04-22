241c8f57a5a9705963fd1144134de17f8f481b9b
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-22 22:08:42 +0200

    Feat: adjust space

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
-    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex item-center justify-between">
+    <nav className="py-6 sm:px-16 lg:px-24 px-4 flex item-center justify-between">
       <div>
         <LinksDropdown />
       </div>
