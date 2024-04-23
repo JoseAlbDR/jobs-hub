c9022eee1efdba961e534415b19cabeeba0440af
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-23 21:08:05 +0200

    Feat: change layout about card

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -15,6 +15,7 @@ import JobInfo from './JobInfo';
 import {
   Briefcase,
   CalendarDays,
+  CalendarDaysIcon,
   Car,
   LinkIcon,
   MapPin,
@@ -23,44 +24,61 @@ import {
 } from 'lucide-react';
 import { Separator } from './ui/separator';
 import { Badge } from './ui/badge';
+import { IconBuildingSkyscraper, IconClockHour2, IconFileCertificate, IconFileDescription, IconLink } from '@tabler/icons-react';
+import { buttonClassName, sectionClassName } from '@/utils/tagStylesConfig';
+import { iconStatus } from '@/utils/iconStatus';
 
 const JobCard = ({ job }: { job: JobData }) => {
   const date = new Date(job.createdAt).toLocaleDateString();
 
+
   return (
     <Card className="bg-muted flex flex-col gap-3">
       <CardHeader>
-        <CardTitle className="capitalize">{job.position}</CardTitle>
-        <CardDescription className="capitalize">{job.company}</CardDescription>
+         <JobInfo text={date} icon={<CalendarDaysIcon className='stroke-primary-accent stroke-1'/>} className='justify-end  '  /> 
+        <Badge className="w-32 justify-center self-start" variant={job.status}>
+          <JobInfo icon={iconStatus(job.status)} text={job.status} />
+        </Badge>
+        <CardTitle className="capitalize text-2xl">{job.position}</CardTitle>
+        <CardDescription className="capitalize flex justify-between flex-col">
+          
+          {job.link 
+        ?<a
+              href={job.link}
+              target="_blank"
+              className='flex items-center gap-1'>
+             <IconLink stroke={1} className='stroke-primary-accent'/> {job.company}
+            </a>
+        : job.company
+          } 
+         
+          </CardDescription>
+          
       </CardHeader>
       <Separator />
       <CardContent className="flex gap-2 flex-col flex-1 pt-6">
-        <div className="flex flex-wrap gap-3 items-center justify-between">
-          <JobInfo icon={<Briefcase />} text={job.mode} />
-          <JobInfo icon={<MapPin />} text={job.location} />
-          <JobInfo icon={<CalendarDays />} text={date} />
-          {job.link && (
-            <a
-              href={job.link}
-              target="_blank"
-              className="flex items-center justify-center gap-1"
-            >
-              <LinkIcon />{' '}
-              <span className="capitalize">{job.link.split('.').at(1)}</span>
-            </a>
-          )}
-          <JobInfo icon={<Car />} text={job.type} />
-        </div>
-        <Badge className="w-32 justify-center" variant={job.status}>
-          <JobInfo icon={<RadioTower />} text={job.status} />
-        </Badge>
-        {job?.note && (
-          <JobInfo icon={<NotebookText />} text={job.note} truncate />
+        <section>
+          {job?.note && (
+          <JobInfo icon={<IconFileDescription stroke={1}  className='stroke-primary-accent'/>} text={job.note} truncate />
         )}
+        </section>
+        <section className='flex justify-between'>
+          <JobInfo icon={<IconClockHour2 stroke={1}  className='stroke-primary-accent'/>} text={job.mode} />
+          <JobInfo icon={<IconFileCertificate stroke={1} className='stroke-primary-accent'/>} text={job.contract} />
+          
+        </section>
+        <section className='flex justify-between'>
+          <JobInfo icon={<IconBuildingSkyscraper stroke={1}  className='stroke-primary-accent'/>} text={job.type} />
+<JobInfo icon={<MapPin  className='stroke-primary-accent stroke-1'/>} text={job.location} />
+        </section>
+        
+        
+        
+        
       </CardContent>
-      <Separator />
-      <CardFooter className="flex gap-4">
-        <Button asChild>
+      
+      <CardFooter className="flex gap-4 justify-end border-t-2 border-primary-accent pt-5">
+        <Button asChild className={'border-2 border-primary-accent bg-transparent rounded-sm'} variant='outline'>
           <Link href={`/jobs/${job.id}`}>Editar</Link>
         </Button>
         <DeleteJobBtn id={job.id} />
