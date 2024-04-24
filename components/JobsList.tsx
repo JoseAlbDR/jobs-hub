123697ada3379f6cdd3a8a1d8a03c2891b006341
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-24 20:17:47 +0200

    fix: change layout

--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -34,7 +34,7 @@ const JobsList = () => {
           <Skeleton className="text-xl font-semibold capitalize mt-4 h-[25px] w-[250px]" />
           <Skeleton className="mt-4 h-[36px] w-[450px]" />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8 mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 mt-8">
           <JobLoadingCard />
           <JobLoadingCard />
           <JobLoadingCard />
@@ -59,7 +59,7 @@ const JobsList = () => {
         </header>
        
       
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 my-5 mx-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 my-5 mx-5">
         {jobs.map((job) => (
           <JobCard key={job.id} job={job} />
         ))}
