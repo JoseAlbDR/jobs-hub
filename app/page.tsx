9afa8e4153a97994fee76190302d2bdfdda73793
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-24 21:08:46 +0200

    Feat: add automatic select logo and image color for color theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+'use client';
+import * as React from 'react';
 import Image from 'next/image';
-import Logo from '../assets/horizontal-black.svg';
-import LandingImg from '../assets/main-black.svg';
+import LogoBlack from '../assets/horizontal-black.svg';
+import LogoWhite from '../assets/horizontal-white.svg';
+import LandingImgBlack from '../assets/main-black.svg';
+import LandingImgWhite from '../assets/main-white.svg';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { buttonClassName } from '@/utils/tagStylesConfig';
+import { useTheme } from 'next-themes';
+import { getBackgroundColor } from '@/utils/getBackgroundColor';
 export default function Home() {
+    const { setTheme, theme } = useTheme();
+     
   
   return (
    
@@ -13,7 +21,7 @@ export default function Home() {
        
         <article className='w-10/12 '>
           <Image
-          src={Logo}
+          src={getBackgroundColor(theme) ==='dark'? LogoWhite :LogoBlack}
           alt="app logo"
           className="my-5 self-center"
           width={250}
@@ -24,15 +32,12 @@ export default function Home() {
             trabajos
           </h1>
           <p className="leading-loose max-w-lg mt-4 text-pretty">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-            expedita voluptatibus, cumque libero et nesciunt itaque omnis quidem
-            aliquam, unde cupiditate! Pariatur, quidem aperiam tempora excepturi
-            similique eius fugit quas!
+            Bienvenido a JobsHub, tu herramienta todo en uno para registrar las ofertas de trabajo a las que has aplicado. Simplifica el seguimiento de tus solicitudes y avanza en tu búsqueda laboral de manera ordenada. ¡Registra tus aplicaciones hoy y mantén el control de tu progreso profesional!
           </p>
          
         </article>
         <article className='w-1/2 flex justify-end'>
- <Image src={LandingImg} alt="landing img" className=" hidden self-end lg:block w-10/12 " />
+ <Image src={getBackgroundColor(theme)==='dark'? LandingImgWhite : LandingImgBlack} alt="landing img" className=" hidden self-end lg:block w-10/12 " />
         </article>
       </section>
       <section><Button asChild className={`${buttonClassName} mt-5 w-56 h-16 text-3xl font-bold rounded-lg`}>
