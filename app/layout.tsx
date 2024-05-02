f5b48c8780ba9685e3e1e23d46a858dce302d536
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-02 10:48:35 +0200

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,33 @@ import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'JobsHub',
   description: 'Aplicación para gestionar tus aplicaciones a ofertas de empleo',
+  keywords: [
+    'JobsHub',
+    'empleo',
+    'aplicaciones',
+    'gestión de empleo',
+    'Next.js',
+    'Prisma',
+    'PostgreSQL',
+    'React.js',
+    'Tailwind CSS',
+    'TypeScript',
+  ],
+  author: 'Jose Alberto Delgado, Juan Manuel Acosta',
+  robots: 'index, follow',
+  ogTitle:
+    'JobsHub - Aplicación para gestionar tus aplicaciones a ofertas de empleo',
+  ogDescription:
+    'JobsHub es una aplicación para gestionar tus búsquedas de empleo de manera eficiente. Registra tus aplicaciones, haz seguimiento de su estado y obtén estadísticas útiles sobre tu progreso.',
+  ogType: 'website',
+  ogUrl: 'https://jobshub.jjteams.dev',
+  twitterTitle:
+    'JobsHub - Aplicación para gestionar tus aplicaciones a ofertas de empleo',
+  twitterDescription:
+    'JobsHub es una aplicación para gestionar tus búsquedas de empleo de manera eficiente. Registra tus aplicaciones, haz seguimiento de su estado y obtén estadísticas útiles sobre tu progreso.',
 };
 
 export default function RootLayout({
@@ -20,7 +44,7 @@ export default function RootLayout({
   return (
     <ClerkProvider localization={esES}>
       <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.className} ` }>
+        <body className={`${inter.className} `}>
           <Providers>{children}</Providers>
         </body>
       </html>
